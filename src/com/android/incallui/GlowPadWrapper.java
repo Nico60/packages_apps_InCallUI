bb7ba21e3879ad28ef2e20f6c89e9c97bc1384af
Jubakuba <Jubakuba@gmail.com>
2014-05-21 10:06:39 -0600

    InCallUI: Add "Answer Via Speakerphone" Glowpad Target
    Change-Id: If8bdb14f1df386272c9b16816175322ecda2fa90
    
    Conflicts:
    
    	res/values/cm_strings.xml
    	res/values/dimens.xml
    
    Conflicts:
    	res/values/dimens.xml
    	src/com/android/incallui/GlowPadWrapper.java

--- src/com/android/incallui/GlowPadWrapper.java
+++ src/com/android/incallui/GlowPadWrapper.java
@@ -27,6 +27,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import com.android.incallui.widget.multiwaveview.GlowPadView;
+import com.android.services.telephony.common.AudioMode;
 import com.android.services.telephony.common.CallDetails;
 
 /**
@@ -125,6 +126,9 @@ public class GlowPadWrapper extends GlowPadView implements GlowPadView.OnTrigger
             case R.drawable.ic_lockscreen_answer:
                 callType = CallDetails.CALL_TYPE_VOICE;
                 break;
+            case R.drawable.ic_lockscreen_speaker:
+                callType = CallDetails.CALL_TYPE_VOICE;
+                break;
             default:
                 Log.wtf(this, "Unknown resource id, resId=" + resId);
                 break;
@@ -150,6 +154,11 @@ public class GlowPadWrapper extends GlowPadView implements GlowPadView.OnTrigger
                 break;
             case R.drawable.ic_lockscreen_text:
                 mAnswerListener.onText();
+                mTargetTriggered = true;
+                break;
+            case R.drawable.ic_lockscreen_speaker:
+                mAnswerListener.onAnswer(toCallType(resId));
+                CallCommandClient.getInstance().setAudioMode(AudioMode.SPEAKER);
                 mTargetTriggered = true;
                 break;
             default:
